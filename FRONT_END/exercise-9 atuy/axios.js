e497dd8132fc98aad58336a2d17afc526762c868
Thimotthy Reymar <129195874+Thimothy25@users.noreply.github.com>
2025-03-10 22:05:18 +0800

    Delete FRONT_END directory

--- FRONT_END/exercise-9 atuy/axios.js
+++
@@ -1,9 +0,0 @@
-const ambilData2 = () => {
-  axios.get("https://reqres.in/api/users").then((result) => {
-    result.data.data.forEach(({ first_name, last_name }) => {
-      console.log(`${first_name},${last_name}`);
-    });
-  });
-};
-ambilData2();
-export { ambilData2 };
--- FRONT_END/exercise-9 atuy/index.js
+++
@@ -1,8 +0,0 @@
-import { messages } from "./promise.js";
-import { ambilData } from "./fetch.js";
-import { ambilData1 } from "./async.js";
-import { ambilData2 } from "./axios.js";
-messages();
-ambilData();
-ambilData1();
-ambilData2();

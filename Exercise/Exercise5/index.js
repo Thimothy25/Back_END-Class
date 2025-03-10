30da687f026a6350baedfcb09b4f3acf7b04cadb
Thimothy25 <thimothypongantung27@gmail.com>
2025-03-10 22:10:45 +0800

    adding exercise5

---
+++ Exercise/Exercise5/index.js
@@ -0,0 +1,56 @@
+const http = require("http");
+const { hello, greetings } = require("./helloWorld");
+const moment = require("moment");
+const express = require("express");
+const morgan = require("morgan");
+// const errorhandler = require("errorhandler");
+const app = express();
+const routers = require("./routers");
+const path = require("path");
+const cors = require("cors");
+
+//Middleware
+const log = (req, res, next) => {
+  console.log(
+    moment().format("h:mm:ss a") + " " + req.originalUrl + " " + req.ip
+  );
+  next();
+};
+
+app.use(morgan("tiny"));
+// app.use(errorhandler);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+//Routing
+app.use(routers);
+
+// Routing 404
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: "error",
+    message: "resource tidak ditemukan",
+  });
+});
+
+//  Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    status: "error",
+    message: "terjadi kesalahan pada server",
+  });
+});
+
+const hostname = "127.0.0.1";
+const port = 3000;
+app.listen(port, hostname, () =>
+  console.log(`Server running at http://${hostname}:${port}`)
+);
